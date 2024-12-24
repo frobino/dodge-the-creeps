b87dd090d46663cda8787e15a7a0160a7e60a99d
frobino <frobino@users.noreply.github.com>
2024-12-24 12:36:14 +0100

    Create main scene using Player and Mob

---
+++ main.gd
@@ -0,0 +1,63 @@
+extends Node
+
+# The export below enables mob_scene to be visible in Inspector, so we can
+# connect it to mob.tscn
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Uncomment the following line to test the game
+	# new_game()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_start_timer_timeout() -> void:
+	# Starts the other 2 timers
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_mob_timer_timeout() -> void:
+	# Create a mob instance, pick a random starting location along the Path2D, and set the mob in motion
+	
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
